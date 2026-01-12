5482d8476bf05a1895050de0b43ddfc531bda5e4
Eduardo Lopez <ed98lopez@gmail.com>
2026-01-12 15:47:17 -0800

    added some more styling to Form Progress component to better help user know what steps have been completed and what is the current step they are on."

--- components/FormProgress.tsx
+++ components/FormProgress.tsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Progress } from "@/components/ui/progress";
 import { Item, ItemContent } from "./ui/item";
 import { Button } from "./ui/button";
+import { Check } from "lucide-react";
+
+type StepStatus = "not complete" | "current" | "complete";
+const setStatusStyleClass = (stepIndex, currentIndex) => {
+  if (stepIndex < currentIndex) {
+    return "border-black border";
+  } else if (stepIndex == currentIndex) {
+    return "border-white border";
+  } else {
+    return "";
+  }
+};
 
 const ProgressStep = ({ stepIndex, title, currentIndex }) => {
+  const statusStyle = setStatusStyleClass(stepIndex, currentIndex);
   return (
     <li className="text-center w-24 ">
       <Button
-        variant={currentIndex == stepIndex ? "outline" : "secondary"}
-        className="size-8"
+        variant={currentIndex == stepIndex ? "default" : "secondary"}
+        className={`size-8  ${statusStyle}`}
       >
-        {stepIndex + 1}
+        {stepIndex < currentIndex ? <Check /> : stepIndex + 1}
       </Button>
-      <p className={`${currentIndex == stepIndex && "font-bold"}`}>{title}</p>
+      <p>{title}</p>
     </li>
   );
 };
@@ -20,8 +33,10 @@ const ProgressStep = ({ stepIndex, title, currentIndex }) => {
 const FormProgress = ({ length, currentIndex, steps }) => {
   const value = (currentIndex / (length - 1)) * 100;
 
+  const widthFraction = `w-${length - 1}/${length}`.toString();
+
   return (
-    <Item variant="outline" className="bg-white">
+    <Item variant="default" className=" ">
       <ItemContent className="relative">
         <ul className="flex justify-around w-full z-1">
           {steps.map((step, i) => (
@@ -35,9 +50,7 @@ const FormProgress = ({ length, currentIndex, steps }) => {
         </ul>
         <Progress
           value={value}
-          className={`w-${
-            length - 1
-          }/${length} place-self-center absolute top-3 z-0`}
+          className={`${widthFraction} place-self-center absolute top-3 z-0`}
         />
       </ItemContent>
     </Item>
