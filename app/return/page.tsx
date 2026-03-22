098ba13c737f3a5b1cf72b80e8c65e8c7e95af98
Eduardo Lopez <ed98lopez@gmail.com>
2026-03-22 11:41:18 -0700

    typing strict false

--- app/return/page.tsx
+++ app/return/page.tsx
@@ -2,18 +2,22 @@ import { redirect } from "next/navigation";
 
 import { stripe } from "../../lib/stripe";
 
-export default async function Return({ searchParams }) {
+export default async function Return({
+  searchParams,
+}: {
+  searchParams: Promise<{ session_id?: string }>;
+}) {
   const { session_id } = await searchParams;
 
   if (!session_id)
     throw new Error("Please provide a valid session_id (`cs_test_...`)");
 
-  const {
-    status,
-    customer_details: { email: customerEmail },
-  } = await stripe.checkout.sessions.retrieve(session_id, {
-    expand: ["line_items", "payment_intent"],
-  });
+  const { status, customer_details } = await stripe.checkout.sessions.retrieve(
+    session_id,
+    {
+      expand: ["line_items", "payment_intent"],
+    },
+  );
 
   if (status === "open") {
     return redirect("/");
@@ -24,7 +28,8 @@ export default async function Return({ searchParams }) {
       <section id="success">
         <p>
           We appreciate your business! A confirmation email will be sent to{" "}
-          {customerEmail}. If you have any questions, please email{" "}
+          {customer_details?.email}. If you have any questions, please
+          email{" "}
         </p>
         <a href="mailto:orders@example.com">orders@example.com</a>.
       </section>
